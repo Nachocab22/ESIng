6cbd9828c3bed4b5bdcbabdbe63be5ee8c61a674
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2022-11-23 17:18:01 +0100

    Base de datos creada
    Se ha creado la base de datos y un los métodos de un par de modelos del proyecto

--- esingdb.sql
+++ esingdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 23-11-2022 a las 13:50:02
+-- Tiempo de generación: 23-11-2022 a las 16:16:27
 -- Versión del servidor: 8.0.17
--- Versión de PHP: 7.3.10
+-- Versión de PHP: 8.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,6 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comunicacion`
+--
+
+CREATE TABLE `comunicacion` (
+  `id` int(11) NOT NULL,
+  `cabecera` varchar(64) NOT NULL,
+  `descripcion` varchar(4096) NOT NULL,
+  `descuento` int(11) NOT NULL,
+  `url` varchar(1024) NOT NULL,
+  `id_encargado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuenta`
+--
+
+CREATE TABLE `cuenta` (
+  `IBAN` varchar(34) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `tipo_cuenta` varchar(64) NOT NULL,
+  `saldo` float DEFAULT NULL,
+  `retenciones` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `incidencia`
 --
 
@@ -35,6 +63,49 @@ CREATE TABLE `incidencia` (
   `motivo` varchar(128) NOT NULL,
   `descripcion` text NOT NULL,
   `incidencia` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimiento`
+--
+
+CREATE TABLE `movimiento` (
+  `id` int(11) NOT NULL,
+  `cuenta` varchar(34) NOT NULL,
+  `tarjeta` varchar(16) NOT NULL,
+  `retenido` tinyint(1) NOT NULL,
+  `concepto` varchar(64) DEFAULT NULL,
+  `cantidad` float NOT NULL,
+  `fecha_op` date NOT NULL,
+  `tipo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tarjeta`
+--
+
+CREATE TABLE `tarjeta` (
+  `numero` varchar(16) NOT NULL,
+  `cuenta` varchar(34) NOT NULL,
+  `fecha_cad` date NOT NULL,
+  `cvv` int(11) NOT NULL,
+  `limite_gastos` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_cuenta`
+--
+
+CREATE TABLE `tipo_cuenta` (
+  `nombre` varchar(64) NOT NULL,
+  `comision` float DEFAULT NULL,
+  `caracteristicas` varchar(2048) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -83,12 +154,48 @@ CREATE TABLE `usuario` (
 --
 
 --
+-- Indices de la tabla `comunicacion`
+--
+ALTER TABLE `comunicacion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_encargado` (`id_encargado`);
+
+--
+-- Indices de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD PRIMARY KEY (`IBAN`),
+  ADD KEY `cuenta_ibfk_1` (`id_cliente`),
+  ADD KEY `cuenta_ibfk_2` (`tipo_cuenta`);
+
+--
 -- Indices de la tabla `incidencia`
 --
 ALTER TABLE `incidencia`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_gestor` (`id_gestor`),
   ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indices de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cuenta` (`cuenta`),
+  ADD KEY `tarjeta` (`tarjeta`);
+
+--
+-- Indices de la tabla `tarjeta`
+--
+ALTER TABLE `tarjeta`
+  ADD PRIMARY KEY (`numero`),
+  ADD KEY `cuenta` (`cuenta`);
+
+--
+-- Indices de la tabla `tipo_cuenta`
+--
+ALTER TABLE `tipo_cuenta`
+  ADD PRIMARY KEY (`nombre`);
 
 --
 -- Indices de la tabla `tipo_usuario`
@@ -108,10 +215,22 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comunicacion`
+--
+ALTER TABLE `comunicacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `incidencia`
 --
 ALTER TABLE `incidencia`
   MODIFY `id` int(64) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_usuario`
@@ -124,6 +243,43 @@ ALTER TABLE `tipo_usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `id` int(64) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comunicacion`
+--
+ALTER TABLE `comunicacion`
+  ADD CONSTRAINT `comunicacion_ibfk_1` FOREIGN KEY (`id_encargado`) REFERENCES `usuario` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD CONSTRAINT `cuenta_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `usuario` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `cuenta_ibfk_2` FOREIGN KEY (`tipo_cuenta`) REFERENCES `tipo_cuenta` (`nombre`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `incidencia`
+--
+ALTER TABLE `incidencia`
+  ADD CONSTRAINT `incidencia_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `usuario` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `incidencia_ibfk_2` FOREIGN KEY (`id_gestor`) REFERENCES `usuario` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD CONSTRAINT `movimiento_ibfk_1` FOREIGN KEY (`cuenta`) REFERENCES `cuenta` (`IBAN`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `movimiento_ibfk_2` FOREIGN KEY (`tarjeta`) REFERENCES `tarjeta` (`numero`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Filtros para la tabla `tarjeta`
+--
+ALTER TABLE `tarjeta`
+  ADD CONSTRAINT `tarjeta_ibfk_1` FOREIGN KEY (`cuenta`) REFERENCES `cuenta` (`IBAN`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
