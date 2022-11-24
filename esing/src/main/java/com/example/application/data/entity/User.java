037f74bf1cbb87490d8b4e5d49c3269df7c27aca
Daniel Pérez Viñolo <74100729+DanPerVin@users.noreply.github.com>
2022-11-24 23:37:46 +0100

    Update User.java with the entity's atributes

--- esing/src/main/java/com/example/application/data/entity/User.java
+++ esing/src/main/java/com/example/application/data/entity/User.java
@@ -2,6 +2,8 @@ package com.example.application.data.entity;
 
 import com.example.application.data.Role;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Date;
 import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -10,22 +12,47 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "application_user")
 public class User extends AbstractEntity {
-
+	
+	@NotNull
     private String username;
+	@NotNull
     private String name;
+	//@NotNull
+    private String surname;
+    //@NotNull
+    @Column(length = 9)
+    private String dni;
+    //@NotNull
+    @Email
+    private String email;
+    //@NotNull
+    private String phone;
+    //@NotNull
+    private String address;
+    //@NotNull
+    private Date birthDate;
+    //@Column(columnDefinition = "tinyint default null")
+    //@OneToOne(mappedBy = "user", fetch = FetchType.LAZY, orphanRemoval = false)
+    //private int titular;
     @JsonIgnore
     private String hashedPassword;
+    
+    //Posiblemente podemos editar el modelo de datos para no tener Tipo_Usuario y tener este Set<Role>
     @Enumerated(EnumType.STRING)
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<Role> roles;
     @Lob
     @Column(length = 1000000)
     private byte[] profilePicture;
+    
 
     public String getUsername() {
         return username;
